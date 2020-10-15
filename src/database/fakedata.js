c17705e01fa29915c33e3c66d04d7bea62ba09a9
Schmockel <danielpschmockel@hotmail.com>
2020-10-15 14:34:31 -0300

    criação de um js com fakedata

---
+++ src/database/fakedata.js
@@ -0,0 +1,34 @@
+module.exports[
+    {
+        id=1,
+        lat: "-27.2190475",
+        lng: "-49.652766",
+        name :"Lar das meninas",
+        description: "Presta assitência a crianças de 06 a 16 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1587784002360-a7c35c090720?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1598454444314-28649fcaa0e9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions : "Venha como se sentir a vontade e traga muito amor e paciência pra dar",
+        opening_hours :"Horários de visitas Das 8h até as 18h",
+        open_on_weekends: "1"
+    },
+
+    {
+        id=2,
+        lat: "-27.3190475",
+        lng: "-49.552766",
+        name :"Lar dos meninos",
+        description: "Presta assitência a crianças de 06 a 16 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1587784002360-a7c35c090720?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1598454444314-28649fcaa0e9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions : "Venha como se sentir a vontade e traga muito amor e paciência pra dar",
+        opening_hours :"Horários de visitas Das 6h até as 18h",
+        open_on_weekends: "0"
+    }
+
+]
