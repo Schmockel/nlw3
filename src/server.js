e5ac57e9f2fdcbd7b9c7007db879fe31d9125d5c
Schmockel <danielpschmockel@hotmail.com>
2020-10-15 10:57:41 -0300

    mudanças feitas nos arquivos para compatilibilizar com o servidor(server.js)

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-console.log('opa,to aqui')
+//importar dependencias
+const express = require('express');
+const path = require('path');//pode evitar alguns erros de \ e /
+
+//iniciando o express
+const server = express();
+server
+//utilizando os arquivos estáticos
+.use(express.static('public'))
+//criar rota
+server.get('/', (request, response) => {
+    return response.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+//ligar o server
+server.listen(5500)
