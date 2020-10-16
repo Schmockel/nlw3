d34f12f54a5848987bd2b8981f535309676e0f37
Schmockel <danielpschmockel@hotmail.com>
2020-10-16 14:25:53 -0300

    tudo funcionando como aparenta teoricamente estar

--- src/database/fakedata.js
+++ src/database/fakedata.js
@@ -4,7 +4,8 @@ module.exports = [
         lat: "-27.2190475",
         lng: "-49.652766",
         name : "Lar das meninas",
-        description: "Presta assitência a crianças de 06 a 16 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        about : "Presta assitência a crianças de 06 a 16 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        whatsapp: "987654321",
         images: [
             "https://images.unsplash.com/photo-1587784002360-a7c35c090720?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
 
--- src/database/test.js
+++ src/database/test.js
@@ -4,25 +4,29 @@ const saveOrphanage = require('./saveOrphanage')
 Database.then(async (db) => {
     //inserir dados na tabela
     await saveOrphanage(db,  {
-        lat: "-27.2190475",
-        lng: "-49.642766",
-        name : "Recanto calmo",
-        description: "Presta assitência a crianças de 06 a 16 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        lat: "-27.2290475",
+        lng: "-49.652766",
+        name : "Lar dos meninos calmos",
+        about: "Presta assitência a crianças de 06 a 16 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        whasapp: "987654321",
         images: [
             "https://images.unsplash.com/photo-1587784002360-a7c35c090720?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
 
             "https://images.unsplash.com/photo-1598454444314-28649fcaa0e9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
         ].toString(),
-        instructions : "paz interior",
+        instructions : "Venha como se sentir a vontade e traga muito amor e paciência pra dar",
         opening_hours :"Horários de visitas Das 8h até as 18h",
-        open_on_weekends: "1"
+        open_on_weekends: "0"
     },) 
 
     //consultar dados na tabela
     const selectedOrphanages = await db.all("SELECT * FROM orphanages")
     console.log(selectedOrphanages)
 
-    //consultar apenas 1 orfanato pelo id
+   /* //consultar apenas 1 orfanato pelo id
     const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "3"')
     console.log(orphanage)
+
+    //deletar dado da tabela
+    console.log(await db.run("DELETE FROM orphanages WHERE id = '4'")) */
 })
