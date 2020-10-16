578c1d419b7b69c016f0e34eb1cf97c0a061c6d4
Schmockel <danielpschmockel@hotmail.com>
2020-10-16 18:57:35 -0300

    tudo funcionando como deveria(eu acho,1:10)

--- src/pages.js
+++ src/pages.js
@@ -1,16 +1,38 @@
-const orphanages = require('./database/fakedata.js')
+const Database = require('./database/db');
+const saveOrphanage = require('./database/saveOrphanage');
 
 module.exports = {
     index(req,res) {
         return res.render('index')
     },
 
-    orphanage(req,res){
-        return res.render('orphanage')
+    async orphanage(req,res){
+        
+        const id = req.query.id
+        
+        try {
+            const db = await Database;
+            const orphanage = await db.all(`SELECT * FROM orphanages WHERE id = "${id}"`)
+            
+            
+            return res.render('orphanage',{orphanage:orphanage[0]})
+        } catch (erro) {
+            console.log(erro);
+            return res.send('erro no banco de dados');
+        }
+        
     },
 
-    orphanages(req,res){
-        return res.render('orphanages',{ orphanages } )
+    async orphanages(req,res){
+        try {
+            const db = await Database;
+            const orphanages = await db.all("SELECT * FROM orphanages")
+            return res.render('orphanages',{ orphanages } )
+        } catch (error) {
+            console.log(error)
+            return res.send('erro no banco de dados')
+        }
+        
     },
 
     createOrphanage(req,res){
